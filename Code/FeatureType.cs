40e9f3e42561383e5e4ba7563d814c8028b886a2
Regan Sarwas <regan_sarwas@nps.gov>
2011-08-18 10:34:22 -0800

    Code cleanup per resharper

--- Code/FeatureType.cs
+++ Code/FeatureType.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ESRI.ArcGIS.Geodatabase;
+﻿using ESRI.ArcGIS.Geodatabase;
 using ESRI.ArcGIS.Geometry;
 
 namespace NPS.AKRO.ArcGIS.GpxPlugin
