c886d19b2121b615754cc4e89ced0b30aba48b45
Regan Sarwas <regan_sarwas@nps.gov>
2021-04-01 10:53:24 -0800

    Create Pro Plugin Template

---
+++ ProCode/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GpxPluginPro")]
+[assembly: AssemblyDescription("Gpx File Reader for ArcGIS Pro")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("National Park Service")]
+[assembly: AssemblyProduct("GpxPluginPro")]
+[assembly: AssemblyCopyright("Copyright © National Park Service 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ec4dc8c-8583-4b4f-8b00-9340583f89eb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
