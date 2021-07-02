f2b11585070d481618e9e35d61d775d3ce63046a
Regan Sarwas <regan_sarwas@nps.gov>
2021-07-01 18:01:40 -0800

    Add the GPX file to the map

--- ProCode/GpxAddin/GpxCatalogItem.cs
+++ ProCode/GpxAddin/GpxCatalogItem.cs
@@ -1,5 +1,6 @@
 ﻿using ArcGIS.Core.CIM;
 using ArcGIS.Core.Data;
+using ArcGIS.Core.Data.PluginDatastore;
 using ArcGIS.Core.Geometry;
 using ArcGIS.Desktop.Catalog;
 using ArcGIS.Desktop.Core;
@@ -14,6 +15,7 @@ using ArcGIS.Desktop.Mapping;
 using ESRI.ArcGIS.ItemIndex;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -82,7 +84,30 @@ namespace GpxAddin
             var item = items.OfType<GpxCatalogItem>().FirstOrDefault();
             if (item == null)
                 return;
-            MessageBox.Show($"Selected Custom Item: {item.Name}");
+            //MessageBox.Show($"Selected Custom Item: {item.Name}");
+            QueuedTask.Run(() =>
+            {
+                var conGpx = new PluginDatasourceConnectionPath("GpxPlugin_Datasource",
+                     new Uri(item.Path, UriKind.Absolute));
+                // because a GPX file can have several "feature classes" they are added to a group layer
+                // MapView.Active may be a 2D map or a 3D scene
+                var groupLayer = LayerFactory.Instance.CreateGroupLayer(MapView.Active.Map, 0, Path.GetFileNameWithoutExtension(item.Path));
+                ArcGIS.Core.Geometry.Envelope zoomToEnv = null;
+                using (var pluginGpx = new PluginDatastore(conGpx))
+                {
+                    System.Diagnostics.Debug.Write($"Table: {item.Path}\r\n");
+                    foreach (var tn in pluginGpx.GetTableNames())
+                    {
+                        using (var table = pluginGpx.OpenTable(tn))
+                        {
+                            //Add feature class to the group layer
+                            LayerFactory.Instance.CreateFeatureLayer((FeatureClass)table, groupLayer);
+                            zoomToEnv = ((FeatureClass)table).GetExtent().Clone() as ArcGIS.Core.Geometry.Envelope;
+                        }
+                    }
+                }
+                if (zoomToEnv != null) MapView.Active.ZoomToAsync(zoomToEnv);
+            });
         }
     }
 }
